271e1f541f756b676d4dcf55d2cc69a062a3866b
namratabhaumik <stockn22@gmail.com>
2025-06-29 22:15:21 -0300

    refactor: Added local .env file and modified relevant backend files for local testing

--- expense-tracker-backend/expense_tracker/expense_tracker/settings.py
+++ expense-tracker-backend/expense_tracker/expense_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y=-csc68#&+fc0rl1s^$jd74na^cro+z8jl4rii!exjifm4$cw'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dev-insecure-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
